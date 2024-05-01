24c8bd779a1011e39e1e8976b31da11c7c16adf4
AshtonStephens <ashton.cs.stephens@gmail.com>
2024-05-01 12:35:08 -0400

    Add auto-built Cargo.toml to emily rust crate and remove  script

--- setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# `.generated-sources/emily/Cargo.toml` changes when the build script runs.
-git update-index --assume-unchanged .generated-sources/emily/Cargo.toml
-# to undo: `git update-index --no-assume-unchanged .generated-sources/emily/Cargo.toml`
