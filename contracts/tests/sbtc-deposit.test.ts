9abc0164bf79155822853ba8f88c68e53aaaad61
jesus <jnajera1917@gmail.com>
2024-05-09 08:37:17 -0400

    added getter test

--- contracts/tests/sbtc-deposit.test.ts
+++ contracts/tests/sbtc-deposit.test.ts
@@ -1,16 +1,20 @@
 import {
   alice,
   deposit,
+  registry,
 } from "./helpers";
 import { test, expect, describe } from "vitest";
 import { txOk, filterEvents, rov } from "@clarigen/test";
 import { CoreNodeEventType, cvToValue } from "@clarigen/core";
+import { uintCV } from "../node_modules/@stacks/transactions/dist/clarity/types/intCV";
+import { principalCV } from "../node_modules/@stacks/transactions/dist/clarity/types/principalCV";
+import { tupleCV } from "@stacks/transactions";
 
 
 describe("sBTC deposit contract", () => {
-  describe("complete deposit wrapper placeholder", () => {
+  describe("complete deposit contract setup", () => {
     
-    test("Call complete-deposit-wrapper", () => {
+    test("Call complete-deposit-wrapper placeholder", () => {
       const receipt = txOk(
         deposit.completeDepositWrapper({
           txid: Uint8Array.from([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]),
@@ -22,6 +26,26 @@ describe("sBTC deposit contract", () => {
       );
       expect(receipt.value).toEqual(true);
     });
+
+    test("Call get-complete-deposit placeholder", () => {
+      const receipt0 = txOk(
+        deposit.completeDepositWrapper({
+          txid: Uint8Array.from([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]),
+          voutIndex: 0,
+          amount: 0,
+          recipient: alice,
+        }),
+        alice
+      );
+      const receipt1 = rov(
+        registry.getCompletedDeposit({
+          txid: Uint8Array.from([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]),
+          voutIndex: 0,
+        }),
+        alice
+      );
+      expect(JSON.stringify(receipt1)).toEqual('{"amount":"0","recipient":"ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5"}');
+    })
 
   });
 });
