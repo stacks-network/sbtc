8e9b70a0b05ecceb41a8ee323334458087e2c7c5
Marzi <marzi@trustmachines.co>
2024-05-09 02:19:17 -0400

    Integrate with risk provider with error handling and unit tests

---
+++ blocklist-client/src/common/error.rs
@@ -0,0 +1,89 @@
+use reqwest::StatusCode;
+use serde::Serialize;
+use warp::reject::Reject;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("HTTP request failed with status code {0}: {1}")]
+    HttpRequestError(StatusCode, String),
+
+    #[error("Network error: {0}")]
+    NetworkError(#[from] reqwest::Error),
+
+    #[error("Serialization error: {0}")]
+    SerializationError(#[from] serde_json::Error),
+
+    #[error("Invalid API response structure")]
+    InvalidApiResponse,
+
+    #[error("Invalid risk value provided: {0}")]
+    InvalidRiskValue(String),
+
+    #[error("Unauthorized access - check your API key")]
+    Unauthorized,
+
+    #[error("Resource not found")]
+    NotFound,
+
+    #[error("Not acceptable format requested")]
+    NotAcceptable,
+
+    #[error("Request conflict")]
+    Conflict,
+
+    #[error("Internal server error")]
+    InternalServerError,
+
+    #[error("Service unavailable")]
+    ServiceUnavailable,
+
+    #[error("Request timeout")]
+    RequestTimeout,
+}
+
+impl Error {
+    pub fn as_http_response(&self) -> (StatusCode, String) {
+        match self {
+            Error::HttpRequestError(code, msg) => (*code, msg.clone()),
+            Error::NetworkError(_) => (StatusCode::BAD_GATEWAY, "Network error".to_string()),
+            Error::SerializationError(_) => (
+                StatusCode::BAD_REQUEST,
+                "Error in processing the data".to_string(),
+            ),
+            Error::InvalidApiResponse => (
+                StatusCode::BAD_REQUEST,
+                "Invalid API response structure".to_string(),
+            ),
+            Error::Unauthorized => (
+                StatusCode::UNAUTHORIZED,
+                "Unauthorized access - check your API key".to_string(),
+            ),
+            Error::NotFound => (StatusCode::NOT_FOUND, "Resource not found".to_string()),
+            Error::NotAcceptable => (
+                StatusCode::NOT_ACCEPTABLE,
+                "Not acceptable format requested".to_string(),
+            ),
+            Error::Conflict => (StatusCode::CONFLICT, "Request conflict".to_string()),
+            Error::InternalServerError => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Internal server error".to_string(),
+            ),
+            Error::ServiceUnavailable => (
+                StatusCode::SERVICE_UNAVAILABLE,
+                "Service unavailable".to_string(),
+            ),
+            Error::RequestTimeout => (StatusCode::REQUEST_TIMEOUT, "Request timeout".to_string()),
+            Error::InvalidRiskValue(_) => (
+                StatusCode::BAD_REQUEST,
+                "Invalid API response risk value".to_string(),
+            ),
+        }
+    }
+}
+
+#[derive(Serialize)]
+pub struct ErrorResponse {
+    pub(crate) message: String,
+}
+
+impl Reject for Error {}
