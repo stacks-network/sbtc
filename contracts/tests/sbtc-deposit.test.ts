1260d04bde500939de704c2790a0ee7bc28fe929
jesus <jnajera1917@gmail.com>
2024-05-09 22:23:03 -0400

    setup two of three remaining tests

--- contracts/tests/sbtc-deposit.test.ts
+++ contracts/tests/sbtc-deposit.test.ts
@@ -9,6 +9,42 @@ import { txOk, filterEvents, rov } from "@clarigen/test";
 
 describe("sBTC deposit contract", () => {
   describe("complete deposit contract setup", () => {
+
+    test("Fail complete-deposit-wrapper invalid txid length", () => {
+      const receipt = txOk(
+        deposit.completeDepositWrapper({
+          txid: new Uint8Array(31).fill(0),
+          voutIndex: 0,
+          amount: 0,
+          recipient: alice,
+        }),
+        alice
+      );
+      console.log(receipt.value.isErr);
+    });
+
+    test("Fail complete-deposit-wrapper replay deposit", () => {
+      const receipt0 = txOk(
+        deposit.completeDepositWrapper({
+          txid: new Uint8Array(32).fill(0),
+          voutIndex: 0,
+          amount: 0,
+          recipient: alice,
+        }),
+        alice
+      );
+      expect(receipt0.value).toEqual(true);
+      const receipt1 = txOk(
+        deposit.completeDepositWrapper({
+          txid: new Uint8Array(32).fill(0),
+          voutIndex: 0,
+          amount: 0,
+          recipient: alice,
+        }),
+        alice
+      );
+      expect(receipt1.value).toEqual(true);
+    });
     
     test("Call complete-deposit-wrapper placeholder", () => {
       const receipt = txOk(
