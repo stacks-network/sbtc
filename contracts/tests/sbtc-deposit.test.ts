a87eb7dc92b647f1dc63791039637e82af72685c
jesus <jnajera1917@gmail.com>
2024-05-10 08:52:38 -0400

    added remaining tests

--- contracts/tests/sbtc-deposit.test.ts
+++ contracts/tests/sbtc-deposit.test.ts
@@ -4,14 +4,14 @@ import {
   registry,
 } from "./helpers";
 import { test, expect, describe } from "vitest";
-import { txOk, filterEvents, rov } from "@clarigen/test";
+import { txOk, filterEvents, rov, txErr } from "@clarigen/test";
 
 
 describe("sBTC deposit contract", () => {
-  describe("complete deposit contract setup", () => {
+  describe("complete deposit contract setup (err 300)", () => {
 
     test("Fail complete-deposit-wrapper invalid txid length", () => {
-      const receipt = txOk(
+      const receipt = txErr(
         deposit.completeDepositWrapper({
           txid: new Uint8Array(31).fill(0),
           voutIndex: 0,
@@ -20,10 +20,10 @@ describe("sBTC deposit contract", () => {
         }),
         alice
       );
-      console.log(receipt.value.isErr);
+      expect(receipt.value).toEqual(300n);
     });
 
-    test("Fail complete-deposit-wrapper replay deposit", () => {
+    test("Fail complete-deposit-wrapper replay deposit (err 301)", () => {
       const receipt0 = txOk(
         deposit.completeDepositWrapper({
           txid: new Uint8Array(32).fill(0),
@@ -34,7 +34,7 @@ describe("sBTC deposit contract", () => {
         alice
       );
       expect(receipt0.value).toEqual(true);
-      const receipt1 = txOk(
+      const receipt1 = txErr(
         deposit.completeDepositWrapper({
           txid: new Uint8Array(32).fill(0),
           voutIndex: 0,
@@ -43,7 +43,7 @@ describe("sBTC deposit contract", () => {
         }),
         alice
       );
-      expect(receipt1.value).toEqual(true);
+      expect(receipt1.value).toEqual(301n);
     });
     
     test("Call complete-deposit-wrapper placeholder", () => {
