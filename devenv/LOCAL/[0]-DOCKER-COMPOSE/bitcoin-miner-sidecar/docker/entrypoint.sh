be1df18b9b642d22ef58102b8d5c17ee2371869f
SundarGowtham <gowthamsundar1998@gmail.com>
2024-05-13 14:03:56 -0500

    BTC Miner is able to access BTC RPC API now

--- devenv/LOCAL/[0]-DOCKER-COMPOSE/bitcoin-miner-sidecar/docker/entrypoint.sh
+++ devenv/LOCAL/[0]-DOCKER-COMPOSE/bitcoin-miner-sidecar/docker/entrypoint.sh
@@ -1,24 +1,39 @@
 #!/bin/sh
 # Wait until bitcoin RPC is ready
+
+RPC_ENDPOINT=http://$BITCOIN_RPC_HOST:$BITCOIN_RPC_PORT/
+
+
 echo "checking if bitcoin node is online"
-until curl -f -s -o /dev/null --user devnet:devnet --data-binary '{"jsonrpc": "1.0", "id": "curltest", "method": "getblockcount", "params": []}' -H 'content-type: text/plain;' http://$BITCOIN_RPC_HOST:$BITCOIN_RPC_PORT/
+
+until curl -f -s -o /dev/null -u "$BTC_RPCUSER:$BTC_RPCPASSWORD" --data-binary '{"jsonrpc": "1.0", "id": "curltest", "method": "getblockcount", "params": []}' -H 'content-type: text/plain;' "$RPC_ENDPOINT"
 do
 	echo "bitcoin node is not ready, sleep two seconds"
 	sleep 2
 done
 echo "bitcoin node is ready"
-# Create a named "legacy" wallet named "devnet"
-curl --user devnet:devnet --data-binary '{"jsonrpc": "1.0", "id": "curltest", "method": "createwallet", "params": {"wallet_name":"devnet","avoid_reuse":true,"descriptors":false,"load_on_startup":true}}' -H 'content-type: text/plain;' http://$BITCOIN_RPC_HOST:$BITCOIN_RPC_PORT/
-echo "devnet bitcoin wallet created"
-# Import miner address
-curl --user devnet:devnet --data-binary '{"jsonrpc": "1.0", "id": "curltest", "method": "importaddress", "params": ['''${MINER_ADDRESS}''']}' -H 'content-type: text/plain;' http://$BITCOIN_RPC_HOST:$BITCOIN_RPC_PORT/
-# Mine the first N blocks
-curl --user devnet:devnet --data-binary '{"jsonrpc": "1.0", "id": "curltest", "method": "generatetoaddress", "params": ['''${INIT_BTC_BLOCKS}''', '''${MINER_ADDRESS}''']}' -H 'content-type: text/plain;' http://$BITCOIN_RPC_HOST:$BITCOIN_RPC_PORT/
-echo "mined initial blocks"
+
+echo "--> Create a named 'legacy' wallet named 'devnet' -->"
+curl -u "$BTC_RPCUSER:$BTC_RPCPASSWORD" --data-binary \
+	'{ "jsonrpc": "1.0", "id": "curltest", "method": "createwallet", "params": { "wallet_name":"devnet", "avoid_reuse":true, "descriptors":false, "load_on_startup":true }}' \
+	-H 'content-type: text/plain;' "$RPC_ENDPOINT"
+echo "==> devnet bitcoin wallet created\n\n"
+
+
+echo "--> Import miner address -->"
+curl -u "$BTC_RPCUSER:$BTC_RPCPASSWORD" -d '{"jsonrpc":"1.0","id":"curltext","method":"importaddress","params":["'"$MINER_ADDRESS"'","Label",false]}' -H 'content-type:text/plain;' $RPC_ENDPOINT
+echo "==> Miner address has been imported\n\n"
+
+
+echo "--> Mine the first N blocks -->"
+curl -u "$BTC_RPCUSER:$BTC_RPCPASSWORD" --data-binary '{"jsonrpc": "1.0", "id": "curltest", "method": "generatetoaddress", "params": ['$INIT_BTC_BLOCKS', "'$MINER_ADDRESS'"]}' -H 'content-type: text/plain;' "$RPC_ENDPOINT"
+echo "=> mined initial blocks \n\n"
+
+
 # Mine a single block every 10 seconds
 while true
 do
-	curl --user devnet:devnet --data-binary '{"jsonrpc": "1.0", "id": "curltest", "method": "generatetoaddress", "params": [1, '''${MINER_ADDRESS}''']}' -H 'content-type: text/plain;' http://$BITCOIN_RPC_HOST:$BITCOIN_RPC_PORT/
+	curl -u "$BTC_RPCUSER:$BTC_RPCPASSWORD" --data-binary '{"jsonrpc": "1.0", "id": "curltest", "method": "generatetoaddress", "params": [1, "'$MINER_ADDRESS'"]}' -H 'content-type: text/plain;' "$RPC_ENDPOINT"
 	echo "mined a single block"
 	sleep ${BTC_BLOCK_GEN_TIME}
 done
