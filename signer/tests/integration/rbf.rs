f9f6578b5fd5a79b3ca6fcd581c3f4e1646faabd
djordon <dan.jordon@gmail.com>
2024-05-18 14:50:12 -0400

    Start on flexible RBF test setup

--- signer/tests/integration/rbf.rs
+++ signer/tests/integration/rbf.rs
@@ -1,11 +1,17 @@
 use bitcoin::AddressType;
 use bitcoin::Amount;
 use bitcoin::OutPoint;
+use bitcoincore_rpc::Client;
 use bitcoincore_rpc::RpcApi;
+use rand::distributions::Uniform;
+use rand::Rng;
+use signer::utxo::DepositRequest;
 use signer::utxo::SbtcRequests;
 use signer::utxo::SignerBtcState;
 use signer::utxo::SignerUtxo;
 use signer::utxo::WithdrawalRequest;
+
+use test_case::test_case;
 
 use crate::regtest;
 use crate::utxo_construction::make_deposit_request;
@@ -14,45 +20,92 @@ use regtest::DEPOSITS_LABEL;
 use regtest::SIGNER_ADDRESS_LABEL;
 use regtest::WITHDRAWAL_LABEL;
 
-#[test]
-#[cfg_attr(not(feature = "integration-tests"), ignore)]
-fn transactions_with_rbf_work() {
-    let (rpc, faucet) = regtest::initialize_blockchain();
-    let fee = regtest::BITCOIN_CORE_FALLBACK_FEE.to_sat();
-
-    let signer = Recipient::new(AddressType::P2tr);
+fn generate_depositor(rpc: &Client, faucet: &Recipient, signer: &Recipient) -> (DepositRequest, Recipient) {
     let depositor = Recipient::new(AddressType::P2tr);
-    let withdrawer = Recipient::new(AddressType::P2wpkh);
     let signers_public_key = signer.keypair.x_only_public_key().0;
-    signer.track_address(&rpc, SIGNER_ADDRESS_LABEL);
     depositor.track_address(rpc, DEPOSITS_LABEL);
-    withdrawer.track_address(rpc, WITHDRAWAL_LABEL);
 
     // Start off with some initial UTXOs to work with.
-    faucet.send_to(rpc, 100_000_000, &signer.address);
     faucet.send_to(rpc, 50_000_000, &depositor.address);
     faucet.generate_blocks(&rpc, 1);
 
-    assert_eq!(signer.get_balance(rpc).to_sat(), 100_000_000);
-    assert_eq!(depositor.get_balance(rpc).to_sat(), 50_000_000);
+    let amount = rand::rngs::OsRng.sample(Uniform::new(100_000, 500_000));
 
     // Now lets make a deposit transaction and submit it
     let depositor_utxo = depositor.get_utxos(rpc, None).pop().unwrap();
-    let deposit_amount = 25_000_000;
-
     let (deposit_tx, deposit_request) = make_deposit_request(
         &depositor,
-        deposit_amount,
+        amount,
         &depositor_utxo,
         signers_public_key,
         faucet.keypair.x_only_public_key().0,
     );
     rpc.send_raw_transaction(&deposit_tx).unwrap();
-    faucet.generate_blocks(rpc, 1);
+    (deposit_request, depositor)
+}
 
-    // The depositor's balance should be updated now.
-    let depositor_balance = depositor.get_balance(rpc);
-    assert_eq!(depositor_balance.to_sat(), 50_000_000 - 25_000_000 - fee);
+fn generate_withdrawal(rpc: &Client) -> (WithdrawalRequest, Recipient) {
+    let withdrawer = Recipient::new(AddressType::P2tr);
+    withdrawer.track_address(rpc, WITHDRAWAL_LABEL);
+
+    let req = WithdrawalRequest {
+        amount: rand::rngs::OsRng.sample(Uniform::new(100_000, 250_000)),
+        max_fee: 250_000,
+        address: withdrawer.address.clone(),
+        signer_bitmap: Vec::new(),
+    };
+
+    (req, withdrawer)
+}
+
+struct RbfContext {
+    initial_deposits: usize,
+    initial_withdrawals: usize,
+    initial_fee_rate: u64,
+    rbf_deposits: usize,
+    rbf_withdrawals: usize,
+    rbf_fee_rate: u64,
+}
+
+#[cfg_attr(not(feature = "integration-tests"), ignore)]
+#[test_case(RbfContext {
+    initial_deposits: 0,
+    initial_withdrawals: 0,
+    initial_fee_rate: 0,
+    rbf_deposits: 0,
+    rbf_withdrawals: 0,
+    rbf_fee_rate: 0,
+} ; "new-deposits-same-withdrawals")]
+fn transactions_with_rbf_work(ctx: RbfContext) {
+    let (rpc, faucet) = regtest::initialize_blockchain();
+    let fee = regtest::BITCOIN_CORE_FALLBACK_FEE.to_sat();
+
+    let signer = Recipient::new(AddressType::P2tr);
+    let signers_public_key = signer.keypair.x_only_public_key().0;
+    signer.track_address(&rpc, SIGNER_ADDRESS_LABEL);
+
+    // Start off with some initial UTXOs to work with.
+    faucet.send_to(rpc, 100_000_000, &signer.address);
+
+    let mut depositors: Vec<Recipient> = Vec::new();
+    let deposits = std::iter::repeat_with(|| generate_depositor(rpc, faucet, &signer))
+        .take(ctx.initial_deposits)
+        .map(|(req, depositor)| {
+            depositors.push(depositor);
+            req
+        })
+        .collect();
+
+    let mut withdrawalers: Vec<Recipient> = Vec::new();
+    let withdrawals = std::iter::repeat_with(|| generate_withdrawal(rpc))
+        .take(ctx.initial_withdrawals)
+        .map(|(req, withdrawaler)| {
+            withdrawalers.push(withdrawaler);
+            req
+        })
+        .collect();
+
+    faucet.generate_blocks(&rpc, 1);
     // We deposited the transaction to the signer, but it's not clear to the
     // wallet tracking the signer's address that the deposit is associated
     // with the signer since it's hidden within the merkle tree.
@@ -64,13 +117,8 @@ fn transactions_with_rbf_work() {
 
     // Now build the struct with the outstanding peg-in and peg-out requests.
     let mut requests = SbtcRequests {
-        deposits: vec![deposit_request],
-        withdrawals: vec![WithdrawalRequest {
-            amount: 20000,
-            max_fee: 100_000,
-            address: withdrawer.address,
-            signer_bitmap: Vec::new(),
-        }],
+        deposits,
+        withdrawals,
         signer_state: SignerBtcState {
             utxo: SignerUtxo {
                 outpoint: OutPoint::new(signer_utxo.txid, signer_utxo.vout),
