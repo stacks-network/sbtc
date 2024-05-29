1df926d37b12b9b91dcd35ce48b0c28b4999af49
SundarGowtham <gowthamsundar1998@gmail.com>
2024-05-28 23:42:02 -0500

    added new components to remove-minikube-containers.sh file

--- devenv/local/k8s/remove-minikube-containers.sh
+++ devenv/local/k8s/remove-minikube-containers.sh
@@ -1,2 +1,7 @@
 minikube image rm minikube/bitcoin:v1
 minikube image rm minikube/bitcoin-miner-sidecar:v1
+
+minikube image rm minikube/stacks:v1
+minikube image rm minikube/stacks-api:v1
+minikube image rm minikube/nakamoto-signer:v1
+minikube image rm minikube/stacks-explorer:v1
