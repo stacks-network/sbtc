0f56201f417bb7478ca453310d36c42b6e6434d8
djordon <dan.jordon@gmail.com>
2024-07-02 15:23:40 -0400

    Add todo integration test

--- signer/tests/integration/contracts.rs
+++ signer/tests/integration/contracts.rs
@@ -9,3 +9,9 @@ async fn complete_deposit_wrapper_tx_accepted() {
 async fn accept_withdrawal_request_tx_accepted() {
     // TODO(#264): Add integration test for signing Stacks smart contracts
 }
+
+#[ignore]
+#[tokio::test]
+async fn reject_withdrawal_request_tx_accepted() {
+    // TODO(#264): Add integration test for signing Stacks smart contracts
+}
