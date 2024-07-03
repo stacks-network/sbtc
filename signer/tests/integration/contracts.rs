1c544111734d2b69a1bcaa94c1dec82d6d8ae058
Daniel Jordon <djordon@users.noreply.github.com>
2024-07-02 21:14:36 -0400

    feat: support broadcasting sbtc burn transactions (#281)
    * Add bitvec as a dependency
    * Add code for making the accept-withdrawal-request contract call

--- signer/tests/integration/contracts.rs
+++ signer/tests/integration/contracts.rs
@@ -3,3 +3,9 @@
 async fn complete_deposit_wrapper_tx_accepted() {
     // TODO(#264): Add integration test for signing Stacks smart contracts
 }
+
+#[ignore]
+#[tokio::test]
+async fn accept_withdrawal_request_tx_accepted() {
+    // TODO(#264): Add integration test for signing Stacks smart contracts
+}
