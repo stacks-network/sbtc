5cbdd773f9d1252498638f51541325e0197dc327
djordon <dan.jordon@gmail.com>
2024-07-08 09:22:53 -0400

    Update the comments for the new error variant

--- signer/src/error.rs
+++ signer/src/error.rs
@@ -74,7 +74,9 @@ pub enum Error {
     #[error("Failed to parse the stacks.api portion of the config: {0}")]
     StacksApiConfig(#[source] config::ConfigError),
 
-    /// Error when reading the stacks API part of the config.toml
+    /// This error happens when converting a sepc256k1::PublicKey into a
+    /// blockstack_lib::util::secp256k1::Secp256k1PublicKey. In general it
+    /// shouldn't happen.
     #[error("Could not transform sepc256k1::PublicKey to stacks variant: {0}")]
     StacksPublicKey(&'static str),
 
