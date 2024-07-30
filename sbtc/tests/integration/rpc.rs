c00552cfb9c3229acfa363fd9a40ea64f521962b
djordon <dan.jordon@gmail.com>
2024-07-30 09:39:21 -0400

    Add integration tests

---
+++ sbtc/tests/integration/rpc.rs
@@ -0,0 +1,88 @@
+//! Test the RPC clients
+
+use bitcoin::hashes::Hash;
+use bitcoin::AddressType;
+use sbtc::error::Error;
+use sbtc::testing::regtest;
+use sbtc::testing::regtest::Recipient;
+
+use sbtc::rpc::BitcoinRpcClient;
+use sbtc::rpc::BtcClient;
+use sbtc::rpc::ElectrumClient;
+use test_case::test_case;
+
+#[cfg_attr(not(feature = "integration-tests"), ignore)]
+#[test_case(BtcClient::new(
+        "http://localhost:18443",
+        regtest::BITCOIN_CORE_RPC_USERNAME.to_string(),
+        regtest::BITCOIN_CORE_RPC_PASSWORD.to_string(),
+    )
+    .unwrap(); "bitcoin client")]
+#[test_case(ElectrumClient::new("tcp://localhost:60401", None).unwrap() ; "electrum client")]
+fn btc_client_gets_transactions<C: BitcoinRpcClient>(client: C) {
+    let (rpc, faucet) = regtest::initialize_blockchain();
+    let signer = Recipient::new(AddressType::P2tr);
+
+    // Newly created "recipients" do not have any UTXOs associated with
+    // their address.
+    let balance = signer.get_balance(rpc);
+    assert_eq!(balance.to_sat(), 0);
+
+    // Okay now we send coins to an address from the one address that
+    // coins have been mined to.
+    let outpoint = faucet.send_to(500_000, &signer.address);
+    let vout = outpoint.vout as usize;
+
+    let response = client.get_tx(&outpoint.txid).unwrap();
+    // Let's make sure we got the right transaction
+    assert_eq!(response.tx.compute_txid(), response.txid);
+    assert_eq!(response.tx.compute_txid(), outpoint.txid);
+    assert_eq!(response.tx.output[vout].value.to_sat(), 500_000);
+    // The transaction has not been confirmed, so these should be None.
+    assert!(response.block_hash.is_none());
+    assert!(response.block_time.is_none());
+    assert!(response.confirmations.is_none());
+    assert!(response.in_active_chain.is_none());
+
+    // Now let's confirm it and try again
+    faucet.generate_blocks(1);
+
+    let response = client.get_tx(&outpoint.txid).unwrap();
+    // Let's make sure we got the right transaction
+    assert_eq!(response.tx.compute_txid(), response.txid);
+    assert_eq!(response.tx.compute_txid(), outpoint.txid);
+    assert_eq!(response.tx.output[vout].value.to_sat(), 500_000);
+    // The transaction has been confirmed, so these should be `Some(_)`.
+    assert!(response.block_hash.is_some());
+    assert!(response.block_time.is_some());
+    assert_eq!(response.confirmations, Some(1));
+    // The `in_active_chain` field is tricky, it needs more confirmations
+    // before it is set. Moreover, it only gets set with the electrum
+    // client. Under the hood, electrum looks up the blockhash of the given
+    // txid and makes a getrawtransaction call to bitcoin-core with this
+    // optional blockhash input, and bitcoin-core will only set the
+    // `in_active_chain` field in the response if it has the blockhash
+    // input in the request. So we stop here and just check that it is
+    // still None. If this was the ElectrumClient then after one more
+    // `faucet.generate_blocks(1)` call it would be set to `Some(true)`.
+    assert!(response.in_active_chain.is_none());
+}
+
+#[cfg_attr(not(feature = "integration-tests"), ignore)]
+#[test_case(BtcClient::new(
+        "http://localhost:18443",
+        regtest::BITCOIN_CORE_RPC_USERNAME.to_string(),
+        regtest::BITCOIN_CORE_RPC_PASSWORD.to_string(),
+    )
+    .unwrap(); "bitcoin client")]
+#[test_case(ElectrumClient::new("tcp://localhost:60401", None).unwrap() ; "electrum client")]
+fn btc_client_unsubmitted_tx<C: BitcoinRpcClient>(client: C) {
+    let _ = regtest::initialize_blockchain();
+    let txid = bitcoin::Txid::all_zeros();
+
+    match client.get_tx(&txid).unwrap_err() {
+        Error::GetTransactionBitcoinCore(_, txid1) | Error::GetTransactionElectrum(_, txid1)
+            if txid1 == txid => {}
+        _ => panic!("Incorrect error variants returned"),
+    }
+}
---
+++ sbtc/tests/integration/validation.rs
@@ -0,0 +1,75 @@
+//! Test deposit validation against bitcoin-core
+
+use bitcoin::AddressType;
+use bitcoin::OutPoint;
+use bitcoin::ScriptBuf;
+use bitcoin::Sequence;
+use bitcoin::TxIn;
+use bitcoin::Witness;
+use bitcoincore_rpc::RpcApi;
+
+use sbtc::deposits::CreateDepositRequest;
+use sbtc::rpc::BitcoinRpcClient;
+use sbtc::rpc::BtcClient;
+use sbtc::rpc::ElectrumClient;
+use sbtc::testing::deposits::TxSetup;
+use sbtc::testing::regtest;
+use sbtc::testing::regtest::Recipient;
+
+use test_case::test_case;
+
+/// Test the CreateDepositRequest::validate function.
+///
+/// We check that we can validate a transaction in the mempool using the
+/// electrum and bitcoin-core clients
+#[cfg_attr(not(feature = "integration-tests"), ignore)]
+#[test_case(BtcClient::new(
+        "http://localhost:18443",
+        regtest::BITCOIN_CORE_RPC_USERNAME.to_string(),
+        regtest::BITCOIN_CORE_RPC_PASSWORD.to_string(),
+    )
+    .unwrap(); "bitcoin client")]
+#[test_case(ElectrumClient::new("tcp://localhost:60401", None).unwrap() ; "electrum client")]
+fn tx_validation_from_mempool<C: BitcoinRpcClient>(client: C) {
+    let max_fee: u64 = 15000;
+    let amount_sats = 49_900_000;
+    let lock_time = 150;
+
+    let mut setup: TxSetup = sbtc::testing::deposits::tx_setup(lock_time, max_fee, amount_sats);
+
+    let (rpc, faucet) = regtest::initialize_blockchain();
+    let depositor = Recipient::new(AddressType::P2tr);
+
+    // Start off with some initial UTXOs to work with.
+    let outpoint = faucet.send_to(50_000_000, &depositor.address);
+    faucet.generate_blocks(1);
+
+    // There is only one UTXO under the depositor's name, so let's get it
+    let utxos = depositor.get_utxos(rpc, None);
+
+    setup.tx.input = vec![TxIn {
+        previous_output: outpoint,
+        sequence: Sequence::ZERO,
+        script_sig: ScriptBuf::new(),
+        witness: Witness::new(),
+    }];
+
+    let request = CreateDepositRequest {
+        outpoint: OutPoint::new(setup.tx.compute_txid(), 0),
+        reclaim_script: setup.reclaim.reclaim_script(),
+        deposit_script: setup.deposit.deposit_script(),
+    };
+
+    regtest::p2tr_sign_transaction(&mut setup.tx, 0, &utxos, &depositor.keypair);
+    rpc.send_raw_transaction(&setup.tx).unwrap();
+
+    let parsed = request.validate(&client).unwrap();
+
+    assert_eq!(parsed.outpoint, request.outpoint);
+    assert_eq!(parsed.deposit_script, request.deposit_script);
+    assert_eq!(parsed.reclaim_script, request.reclaim_script);
+    assert_eq!(parsed.amount, amount_sats);
+    assert_eq!(parsed.signers_public_key, setup.deposit.signers_public_key);
+    assert_eq!(parsed.lock_time, lock_time as u64);
+    assert_eq!(parsed.recipient, setup.deposit.recipient);
+}
