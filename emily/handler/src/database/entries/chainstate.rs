b045d22d0f7d458554bcc219d42e41a3ffb6b87c
Ashton Stephens <ashton.cs.stephens@gmail.com>
2024-08-12 13:50:15 -0400

    feat: Generalize Emily DynamoDB Table Accessors (#419)
    * feat: Alter dynamodb accessor code to specify table details once.
    
    * nits: Respond to good style advice

--- emily/handler/src/database/entries/chainstate.rs
+++ emily/handler/src/database/entries/chainstate.rs
@@ -9,6 +9,8 @@ use crate::api::models::{
     common::{BlockHeight, StacksBlockHash},
 };
 
+use super::{EntryTrait, KeyTrait, PrimaryIndex, PrimaryIndexTrait};
+
 // Chainstate entry ---------------------------------------------------------------
 
 /// Chainstate table entry key. This is the primary index key.
@@ -52,6 +54,43 @@ impl From<Chainstate> for ChainstateEntry {
     }
 }
 
+/// Implements the key trait for the deposit entry key.
+impl KeyTrait for ChainstateEntryKey {
+    /// The type of the partition key.
+    type PartitionKey = BlockHeight;
+    /// the type of the sort key.
+    type SortKey = StacksBlockHash;
+    /// The table field name of the partition key.
+    const PARTITION_KEY_NAME: &'static str = "Height";
+    /// The table field name of the sort key.
+    const _SORT_KEY_NAME: &'static str = "Hash";
+}
+
+/// Implements the entry trait for the deposit entry.
+impl EntryTrait for ChainstateEntry {
+    /// The type of the key for this entry type.
+    type Key = ChainstateEntryKey;
+    /// Extract the key from the deposit entry.
+    fn key(&self) -> Self::Key {
+        ChainstateEntryKey {
+            height: self.key.height,
+            hash: self.key.hash.clone(),
+        }
+    }
+}
+
+/// Primary index struct.
+pub struct ChainstateTablePrimaryIndexInner;
+/// Withdrawal table primary index type.
+pub type ChainstateTablePrimaryIndex = PrimaryIndex<ChainstateTablePrimaryIndexInner>;
+/// Definition of Primary index trait.
+impl PrimaryIndexTrait for ChainstateTablePrimaryIndexInner {
+    type Entry = ChainstateEntry;
+    fn table_name(settings: &crate::context::Settings) -> &str {
+        &settings.chainstate_table_name
+    }
+}
+
 // Api State Entry -------------------------------------------------------------
 
 /// Special hash value for the chainstate entry that stores information about the
@@ -68,7 +107,7 @@ const API_STATE_HEIGHT_TOKEN: i32 = -1;
 /// special entry nearly impossible for any regular access path.
 #[derive(Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
-struct SpecialApiStateKey {
+pub struct SpecialApiStateKey {
     /// Special token that takes the place of the hash field. Constant fields
     /// are set in the `Default` function for this struct.
     #[serde(rename = "Hash")]
@@ -122,8 +161,44 @@ pub struct ApiStateEntry {
 
 impl ApiStateEntry {
     /// Get the special key.
-    pub fn key() -> serde_json::Value {
-        serde_json::to_value(SpecialApiStateKey::default())
-            .expect("Default key cannot fail to serialize.")
+    pub fn key() -> SpecialApiStateKey {
+        SpecialApiStateKey {
+            api_state_token: API_STATE_HASH_TOKEN.to_string(),
+            negative_one: API_STATE_HEIGHT_TOKEN,
+        }
+    }
+}
+
+/// Implements the key trait for the deposit entry key.
+impl KeyTrait for SpecialApiStateKey {
+    /// The type of the partition key.
+    type PartitionKey = i32;
+    /// the type of the sort key.
+    type SortKey = String;
+    /// The table field name of the partition key.
+    const PARTITION_KEY_NAME: &'static str = "Height";
+    /// The table field name of the sort key.
+    const _SORT_KEY_NAME: &'static str = "Hash";
+}
+
+/// Implements the entry trait for the deposit entry.
+impl EntryTrait for ApiStateEntry {
+    /// The type of the key for this entry type.
+    type Key = SpecialApiStateKey;
+    /// Extract the key from the deposit entry.
+    fn key(&self) -> Self::Key {
+        ApiStateEntry::key()
+    }
+}
+
+/// Primary index struct.
+pub struct SpecialApiStateIndexInner;
+/// Withdrawal table primary index type.
+pub type SpecialApiStateIndex = PrimaryIndex<SpecialApiStateIndexInner>;
+/// Definition of Primary index trait.
+impl PrimaryIndexTrait for SpecialApiStateIndexInner {
+    type Entry = ApiStateEntry;
+    fn table_name(settings: &crate::context::Settings) -> &str {
+        &settings.chainstate_table_name
     }
 }
--- emily/handler/src/database/entries/mod.rs
+++ emily/handler/src/database/entries/mod.rs
@@ -1,4 +1,57 @@
-//! Module with the entries that go into the database.
+//! This module defines the structures, traits, and implementations necessary for managing
+//! entries within a DynamoDB database. It provides generic abstractions for handling different
+//! types of database entries, including their indexing, retrieval, insertion, and deletion
+//! operations.
+//!
+//! ## Module Overview
+//!
+//! The module is divided into several submodules and components:
+//!
+//! - **Submodules**:
+//!   - `chainstate`: Handles entries related to the chain state table.
+//!   - `deposit`: Manages entries for the deposit table.
+//!   - `withdrawal`: Manages entries for the withdrawal table.
+//!
+//! - **Structures**:
+//!   - `PrimaryIndex<T>`: Wrapper for primary index structures.
+//!   - `SecondaryIndex<T>`: Wrapper for secondary index structures.
+//!
+//! - **Traits**:
+//!   - `EntryTrait`: A trait common to all database entries, defining how entries are keyed.
+//!   - `KeyTrait`: Defines the structure of a table key, including partition and sort keys.
+//!   - `PrimaryIndexTrait`: Used for defining a primary index and its associated table operations.
+//!   - `SecondaryIndexTrait`: Extends the `PrimaryIndexTrait` to support secondary indexes.
+//!   - `TableIndexTrait`: A base trait that defines common table operations across indexes.
+//!
+//! - **Implementations**:
+//!   - Generic implementations for handling database operations such as get, query, put, and delete
+//!     across different types of indexes.
+//!
+//! ## Database Operations
+//!
+//! This module abstracts the complexity of interacting with DynamoDB by providing easy-to-use
+//! methods for common operations such as:
+//!
+//! - **Retrieval**: Fetch entries by key or query based on a partition key.
+//! - **Insertion**: Add new entries to the database.
+//! - **Deletion**: Remove entries from the database either by key or in bulk.
+//! - **Scanning**: Retrieve all entries from a table.
+//! - **Table Management**: Wipe a table clean for testing or other purposes.
+//!
+//! ## Usage
+//!
+//! This module is intended for internal use within the application and is designed to be flexible
+//! enough to support various data models while maintaining strong type safety and clear separation
+//! of concerns.
+
+use std::collections::HashMap;
+
+use aws_sdk_dynamodb::types::{AttributeValue, DeleteRequest, WriteRequest};
+use base64::{engine::general_purpose::URL_SAFE_NO_PAD, Engine};
+use serde::{Deserialize, Serialize};
+use serde_dynamo::Item;
+
+use crate::{common::error::Error, context::Settings};
 
 /// Chainstate table entries.
 pub mod chainstate;
@@ -6,3 +59,376 @@ pub mod chainstate;
 pub mod deposit;
 /// Withdrawal table entries.
 pub mod withdrawal;
+
+// Structures
+// -----------------------------------------------------------------------------
+
+/// Primary index wrapper struct.
+pub struct PrimaryIndex<T>(pub T);
+
+/// Secondary index wrapper struct.
+pub struct SecondaryIndex<T>(pub T);
+
+// Traits
+// -----------------------------------------------------------------------------
+
+/// Trait common to entries.
+pub trait EntryTrait: serde::Serialize + for<'de> serde::Deserialize<'de> {
+    /// Key type for the entry.
+    type Key: KeyTrait;
+    /// Retrieves the entry key.
+    fn key(&self) -> Self::Key;
+}
+
+/// Table key.
+pub trait KeyTrait: serde::Serialize + for<'de> serde::Deserialize<'de> {
+    /// Partition key type.
+    type PartitionKey: serde::Serialize + for<'de> serde::Deserialize<'de>;
+    /// Sort key type.
+    type SortKey: serde::Serialize + for<'de> serde::Deserialize<'de>;
+    /// Parition key name.
+    const PARTITION_KEY_NAME: &'static str;
+    /// Sort key name.
+    const _SORT_KEY_NAME: &'static str;
+}
+
+/// Trait that defines a primary index.
+pub(crate) trait PrimaryIndexTrait {
+    /// Entry type.
+    type Entry: EntryTrait;
+    /// Gets table name.
+    fn table_name(settings: &Settings) -> &str;
+}
+
+/// Trait for defining a secondary index.
+pub(crate) trait SecondaryIndexTrait {
+    /// Primary Index.
+    type PrimaryIndex: TableIndexTrait;
+    /// Entry type.
+    type Entry: EntryTrait;
+    /// Index name.
+    const INDEX_NAME: &'static str;
+}
+
+// Implementations for underlying trait.
+// -----------------------------------------------------------------------------
+
+/// Implementation of the table index trait for Primary Index.
+impl<T> TableIndexTrait for PrimaryIndex<T>
+where
+    T: PrimaryIndexTrait,
+{
+    type Entry = <T as PrimaryIndexTrait>::Entry;
+    type PrimaryIndex = PrimaryIndex<T>;
+    type SearchToken = <<T as PrimaryIndexTrait>::Entry as EntryTrait>::Key;
+    const INDEX_NAME_IF_GSI: Option<&'static str> = None;
+    fn table_name(settings: &Settings) -> &str {
+        <T as PrimaryIndexTrait>::table_name(settings)
+    }
+}
+
+/// Implementation of the table index trait for Primary Index.
+impl<T> TableIndexTrait for SecondaryIndex<T>
+where
+    T: SecondaryIndexTrait,
+{
+    type Entry = <T as SecondaryIndexTrait>::Entry;
+    type PrimaryIndex = <T as SecondaryIndexTrait>::PrimaryIndex;
+    type SearchToken = SecondaryIndexSearchToken<
+        <<<T as SecondaryIndexTrait>::PrimaryIndex as TableIndexTrait>::Entry as EntryTrait>::Key,
+        <<T as SecondaryIndexTrait>::Entry as EntryTrait>::Key,
+    >;
+    const INDEX_NAME_IF_GSI: Option<&'static str> = Some(<T as SecondaryIndexTrait>::INDEX_NAME);
+    fn table_name(settings: &Settings) -> &str {
+        <<T as SecondaryIndexTrait>::PrimaryIndex as TableIndexTrait>::table_name(settings)
+    }
+}
+
+// Base Trait
+// -----------------------------------------------------------------------------
+
+/// Table index trait.
+pub(crate) trait TableIndexTrait {
+    /// Entry type.
+    type Entry: EntryTrait;
+    /// Search token.
+    type SearchToken: serde::Serialize + for<'de> serde::Deserialize<'de>;
+    /// Primary index trait.
+    type PrimaryIndex: TableIndexTrait;
+    /// Index name if the index is a GSI.
+    const INDEX_NAME_IF_GSI: Option<&'static str>;
+    /// Gets table name.
+    fn table_name(settings: &Settings) -> &str;
+
+    /// Generic table get.
+    async fn get_entry(
+        dynamodb_client: &aws_sdk_dynamodb::Client,
+        settings: &Settings,
+        key: &<Self::Entry as EntryTrait>::Key,
+    ) -> Result<Self::Entry, Error> {
+        // Convert key into the type needed for querying.
+        let key_item: serde_dynamo::Item = serde_dynamo::to_item(key)?;
+        // Query the database.
+        let get_item_output = dynamodb_client
+            .get_item()
+            .table_name(Self::table_name(settings))
+            .set_key(Some(key_item.into()))
+            .send()
+            .await?;
+        // Get DynamoDB item.
+        let item = get_item_output.item.ok_or(Error::NotFound)?;
+        // Convert item into entry.
+        let entry = serde_dynamo::from_item(item)?;
+        // Return.
+        Ok(entry)
+    }
+
+    /// Generic table query for all attributes with a given primary key.
+    async fn query_with_partition_key(
+        dynamodb_client: &aws_sdk_dynamodb::Client,
+        settings: &Settings,
+        partition_key: &<<Self::Entry as EntryTrait>::Key as KeyTrait>::PartitionKey,
+        maybe_next_token: Option<String>,
+        maybe_page_size: Option<i32>,
+    ) -> Result<(Vec<Self::Entry>, Option<String>), Error> {
+        // Convert inputs into the types needed for querying.
+        let exclusive_start_key =
+            maybe_exclusive_start_key_from_next_token::<Self::SearchToken>(maybe_next_token)?;
+        // Query the database.
+        let query_output = dynamodb_client
+            .query()
+            .table_name(Self::table_name(settings))
+            .set_index_name(Self::INDEX_NAME_IF_GSI.map(|s| s.to_string()))
+            .set_exclusive_start_key(exclusive_start_key)
+            .set_limit(maybe_page_size)
+            .key_condition_expression("#pk = :v")
+            .expression_attribute_names(
+                "#pk",
+                <<Self::Entry as EntryTrait>::Key as KeyTrait>::PARTITION_KEY_NAME,
+            )
+            .expression_attribute_values(":v", serde_dynamo::to_attribute_value(partition_key)?)
+            .send()
+            .await?;
+        // Convert data into output format.
+        let entries: Vec<Self::Entry> =
+            serde_dynamo::from_items(query_output.items.unwrap_or_default())?;
+        let next_token = maybe_next_token_from_last_evaluated_key::<Self::SearchToken>(
+            query_output.last_evaluated_key,
+        )?;
+        // Return.
+        Ok((entries, next_token))
+    }
+
+    /// Generic put table entry.
+    async fn put_entry(
+        dynamodb_client: &aws_sdk_dynamodb::Client,
+        settings: &Settings,
+        entry: &Self::Entry,
+    ) -> Result<(), Error> {
+        // Get table name.
+        let table_name = Self::table_name(settings);
+        // Convert Entry into the type needed for querying.
+        let entry_item: Item = serde_dynamo::to_item(entry)?;
+        // Add to the database.
+        dynamodb_client
+            .put_item()
+            .table_name(table_name)
+            .set_item(Some(entry_item.into()))
+            .send()
+            .await?;
+        // Return.
+        Ok(())
+    }
+
+    /// Get all entries from a dynamodb table.
+    async fn get_all_entries(
+        dynamodb_client: &aws_sdk_dynamodb::Client,
+        settings: &Settings,
+    ) -> Result<Vec<Self::Entry>, Error> {
+        // Get table name.
+        let table_name = Self::table_name(settings);
+        // Create vector to aggregate items in.
+        let mut all_entries: Vec<Self::Entry> = Vec::new();
+        // Scan the table for as many entries as possible.
+        let mut scan_output = dynamodb_client.scan().table_name(table_name).send().await?;
+        // Put items into aggregate list.
+        all_entries.extend(serde_dynamo::from_items(
+            scan_output.items.unwrap_or_default(),
+        )?);
+        // Continue to query until the scan is done.
+        while let Some(exclusive_start_key) = scan_output.last_evaluated_key {
+            scan_output = dynamodb_client
+                .scan()
+                .table_name(table_name)
+                .set_exclusive_start_key(Some(exclusive_start_key))
+                .send()
+                .await?;
+            all_entries.extend(serde_dynamo::from_items(
+                scan_output.items.unwrap_or_default(),
+            )?);
+        }
+        // Return.
+        Ok(all_entries)
+    }
+
+    /// Generic delete table entry.
+    async fn delete_entry(
+        dynamodb_client: &aws_sdk_dynamodb::Client,
+        settings: &Settings,
+        key: &<Self::Entry as EntryTrait>::Key,
+    ) -> Result<(), Error> {
+        // Get table name.
+        let table_name = Self::table_name(settings);
+        // Convert Entry into the type needed for querying.
+        let key_item: Item = serde_dynamo::to_item(key)?;
+        // Add to the database.'
+        dynamodb_client
+            .delete_item()
+            .table_name(table_name)
+            .set_key(Some(key_item.into()))
+            .send()
+            .await?;
+        // Return.
+        Ok(())
+    }
+
+    /// Deletes every entry in a table with the specified keys.
+    async fn delete_entries(
+        dynamodb_client: &aws_sdk_dynamodb::Client,
+        settings: &Settings,
+        keys_to_delete: Vec<<Self::Entry as EntryTrait>::Key>,
+    ) -> Result<(), Error> {
+        // Get table name.
+        let table_name = Self::table_name(settings);
+        // Make delete request aggregator.
+        let mut write_delete_requests: Vec<WriteRequest> = Vec::new();
+        for key in keys_to_delete {
+            let key_item = serde_dynamo::to_item::<<Self::Entry as EntryTrait>::Key, Item>(key)?;
+            let req = DeleteRequest::builder()
+                .set_key(Some(key_item.into()))
+                .build()?;
+            let write_request = WriteRequest::builder().delete_request(req).build();
+            write_delete_requests.push(write_request);
+        }
+        // Execute the deletes in chunks.
+        for chunk in write_delete_requests.chunks(25) {
+            dynamodb_client
+                .batch_write_item()
+                .request_items(table_name, chunk.to_vec())
+                .send()
+                .await?;
+        }
+        // Return.
+        Ok(())
+    }
+
+    /// Wipes the table.
+    #[cfg(feature = "testing")]
+    async fn wipe(
+        dynamodb_client: &aws_sdk_dynamodb::Client,
+        settings: &Settings,
+    ) -> Result<(), Error> {
+        // Get table name.
+        match Self::INDEX_NAME_IF_GSI {
+            // If this is secondary index, so go to primary:
+            Some(_) => {
+                let keys_to_delete = <Self::PrimaryIndex as TableIndexTrait>::get_all_entries(
+                    dynamodb_client,
+                    settings,
+                )
+                .await?
+                .iter()
+                .map(<<Self::PrimaryIndex as TableIndexTrait>::Entry as EntryTrait>::key)
+                .collect();
+
+                <Self::PrimaryIndex as TableIndexTrait>::delete_entries(
+                    dynamodb_client,
+                    settings,
+                    keys_to_delete,
+                )
+                .await
+            }
+            // Otherwise this is a primary index so wipe the table.
+            None => {
+                Self::delete_entries(
+                    dynamodb_client,
+                    settings,
+                    Self::get_all_entries(dynamodb_client, settings)
+                        .await?
+                        .iter()
+                        .map(<Self::Entry as EntryTrait>::key)
+                        .collect(),
+                )
+                .await
+            }
+        }
+    }
+}
+
+/// Secondary index search token definition.
+#[derive(Clone, Default, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
+pub struct SecondaryIndexSearchToken<P, S> {
+    /// Primary index.
+    #[serde(flatten)]
+    primary: P,
+    /// Secondary index.
+    #[serde(flatten)]
+    secondary: S,
+}
+
+// Private Helpers -------------------------------------------------------------
+
+/// Converts an optional `HashMap<String, AttributeValue>` representing the last evaluated key
+/// into an optional token string. If the `Option` contains a value, it is deserialized into a type `T`
+/// and then serialized into a token string.
+fn maybe_next_token_from_last_evaluated_key<T>(
+    maybe_last_evaluated_key: Option<HashMap<String, AttributeValue>>,
+) -> Result<Option<String>, Error>
+where
+    T: Serialize + for<'de> Deserialize<'de>,
+{
+    let maybe_next_token = maybe_last_evaluated_key
+        .map(|key| serde_dynamo::from_item(key))
+        .transpose()?
+        .map(|token_data: T| tokenize(token_data))
+        .transpose()?;
+    Ok(maybe_next_token)
+}
+
+/// Turns an optional key into a token.
+fn tokenize<T>(key: T) -> Result<String, Error>
+where
+    T: Serialize,
+{
+    let serialied = serde_json::to_string(&key)?;
+    let encoded: String = URL_SAFE_NO_PAD.encode(serialied);
+    Ok(encoded)
+}
+
+/// Converts an optional token string into an optional `HashMap<String, AttributeValue>` representing
+/// the exclusive start key. If the `Option` contains a value, it is deserialized into a type `T`
+/// and then serialized into a `HashMap<String, AttributeValue>`.
+fn maybe_exclusive_start_key_from_next_token<T>(
+    maybe_next_token: Option<String>,
+) -> Result<Option<HashMap<String, AttributeValue>>, Error>
+where
+    T: Serialize + for<'de> Deserialize<'de>,
+{
+    let maybe_exclusive_start_key = maybe_next_token
+        .map(|next_token| detokenize(next_token))
+        .transpose()?
+        .map(|token_data: T| serde_dynamo::to_item(token_data))
+        .transpose()?
+        .map(|token_item: Item| token_item.into());
+    Ok(maybe_exclusive_start_key)
+}
+
+/// Turns an optional token into a key.
+fn detokenize<T>(token: String) -> Result<T, Error>
+where
+    T: for<'de> Deserialize<'de>,
+{
+    let decoded = URL_SAFE_NO_PAD.decode(token)?;
+    let deserialized = serde_json::from_slice::<T>(&decoded)?;
+    Ok(deserialized)
+}
