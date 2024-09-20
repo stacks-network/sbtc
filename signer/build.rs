eb9996babb514a7b81fe9be6d57ec2b710a479d4
djordon <dan.jordon@gmail.com>
2024-09-20 14:42:58 -0400

    Commit generated protobufs

--- signer/build.rs
+++ signer/build.rs
@@ -1,4 +1,22 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("../protobufs/stacks/signer/message_relay.proto")?;
-    Ok(())
+fn main() {
+    compile_protos();
+}
+
+pub fn compile_protos() {
+    let workingdir = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .unwrap();
+
+    let protos = ["protobufs/stacks/signer/message_relay.proto"].map(|path| workingdir.join(path));
+
+    println!("cargo:rerun-if-changed=protobufs/stacks/signer/");
+
+    // Compile protocol buffers
+    tonic_build::configure()
+        .build_server(false)
+        .build_client(false)
+        .out_dir(workingdir.join("signer/src/proto/generated/"))
+        .include_file("mod.rs")
+        .compile(&protos, &[workingdir.join("protobufs")])
+        .expect("Unable to compile protocol buffers");
 }
--- signer/src/lib.rs
+++ signer/src/lib.rs
@@ -13,6 +13,7 @@ pub mod error;
 pub mod keys;
 pub mod message;
 pub mod network;
+pub mod proto;
 pub mod signature;
 pub mod stacks;
 pub mod storage;
