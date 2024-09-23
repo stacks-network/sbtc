95affc4243227b6766760980d70fa1bc48cc17e3
Daniel Jordon <djordon@users.noreply.github.com>
2024-09-23 09:33:55 -0400

    feat: commit generated protobufs (#553)

--- signer/build.rs
+++ signer/build.rs
@@ -1,4 +1,22 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("../protobufs/stacks/signer/message_relay.proto")?;
-    Ok(())
+fn main() {
+    compile_protos();
+}
+
+pub fn compile_protos() {
+    let workingdir = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .unwrap();
+
+    let protos = ["protobufs/stacks/signer/message_relay.proto"].map(|path| workingdir.join(path));
+
+    println!("cargo:rerun-if-changed=protobufs/stacks/signer/");
+
+    // Compile protocol buffers
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .out_dir(workingdir.join("signer/src/proto/generated/"))
+        .include_file("mod.rs")
+        .compile(&protos, &[workingdir.join("protobufs")])
+        .expect("Unable to compile protocol buffers");
 }
