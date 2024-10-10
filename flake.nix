c3548cccc8978f028842d8f8d3ad2b84a2fd73dc
Adriano Di Luzio <adrianodl@hotmail.it>
2024-10-10 15:19:02 +0200

    Revert "Add `smithy-cli` for macOS and Linux"
    This reverts commit 7b8075c2505af2a2b95f2d6ccde100e95f62cfdc.

--- flake.nix
+++ flake.nix
@@ -18,27 +18,6 @@
             inherit system overlays;
           };
           toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-
-          smithy-cli = pkgs.stdenv.mkDerivation rec {
-            smithy-system-hash = {
-              "aarch64-darwin" = [ "darwin-aarch64" "sha256-s/y8turEQfvGwJ2xz70SwAIlg1fk4miOPZ5SapO6tJU=" ];
-              "x86_64-darwin" = [ "darwin-x86_64" "sha256-iTJ4PbFfxG+sU2B4Viu5fG0K44zqcU/I3Y7sTlFPAuQ=" ];
-              "aarch64-linux" = [ "linux-aarch64" "sha256-3SSmT1t5ctgVa3lXnjGRDb2OJ3j272RTUm1aQ1Itjlo=" ];
-              "x86_64-linux" = [ "linux-x86_64" "sha256-TleL3pvFg/LemPFktzUDVI1YYzh78yKtHwuc0l33x1I=" ];
-            }.${system};
-
-            pname = "smithy-cli";
-            version = "1.51.0";
-            base_url = "https://github.com/smithy-lang/smithy/releases/download/";
-            src = pkgs.fetchzip {
-              url = "${base_url}/${version}/smithy-cli-${builtins.elemAt smithy-system-hash 0}.zip";
-              hash = builtins.elemAt smithy-system-hash 1;
-            };
-            buildPhase = ''
-              mkdir $out
-              mv * $out
-            '';
-          };
         in
         with pkgs;
         {
@@ -51,7 +30,6 @@
 
             buildInputs = [
               toolchain
-              smithy-cli
 
               cargo-lambda
               gnumake
