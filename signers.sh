fd310b594227b4e82aad8b22f963a078274d944d
Cyle Witruk <cyle.witruk@outlook.com>
2024-10-16 10:05:49 +0200

    minor readability tweaks in signers.sh

--- signers.sh
+++ signers.sh
@@ -9,6 +9,8 @@ RED='\e[0;31m'          # Red
 NC='\e[0m' # No Color
 BOLD='\e[1m' # Bold
 
+DEVENV="$PWD/devenv/local"
+
 exec_run() {
   if [ "$1" -eq 1 ]; then
     printf "${RED}ERROR:${NC} At least 2 signers are required\n"
@@ -16,12 +18,12 @@ exec_run() {
   fi
 
   printf "${GRAY}Running ${NC}${BOLD}$@${NC} signers\n"
-  SIGNER1_KEY=$(. $PWD/devenv/local/envs/signer-1.env; echo "$SIGNER_PUBKEY")
-  SIGNER2_KEY=$(. $PWD/devenv/local/envs/signer-2.env; echo "$SIGNER_PUBKEY")
-  SIGNER3_KEY=$(. $PWD/devenv/local/envs/signer-3.env; echo "$SIGNER_PUBKEY")
+  SIGNER1_KEY=$(. $DEVENV/envs/signer-1.env; echo "$SIGNER_PUBKEY")
+  SIGNER2_KEY=$(. $DEVENV/envs/signer-2.env; echo "$SIGNER_PUBKEY")
+  SIGNER3_KEY=$(. $DEVENV/envs/signer-3.env; echo "$SIGNER_PUBKEY")
 
-  docker compose -f $PWD/devenv/local/docker-compose/docker-compose.yml down postgres-1 postgres-2 postgres-3 -v
-  docker compose -f $PWD/devenv/local/docker-compose/docker-compose.yml up -d postgres-1 postgres-2 postgres-3
+  docker compose -f $DEVENV/docker-compose/docker-compose.yml down postgres-1 postgres-2 postgres-3 -v
+  docker compose -f $DEVENV/docker-compose/docker-compose.yml up -d postgres-1 postgres-2 postgres-3
 
   if [ "$1" -eq 2 ]; then
     BOOTSTRAP_SIGNER_SET="$SIGNER1_KEY,$SIGNER2_KEY"
@@ -29,15 +31,15 @@ exec_run() {
     BOOTSTRAP_SIGNER_SET="$SIGNER1_KEY,$SIGNER2_KEY,$SIGNER3_KEY"
   fi
 
-  RESULT1=$(. $PWD/devenv/local/envs/signer-1.env && SIGNER_SIGNER__BOOTSTRAP_SIGNING_SET="$BOOTSTRAP_SIGNER_SET" && cargo run --bin sbtc-signer -- --config devenv/local/docker-compose/sbtc-signer/signer-config.toml --migrate-db > $PWD/target/signer-1.log 2>&1 &)
+  RESULT1=$(. $DEVENV/envs/signer-1.env && SIGNER_SIGNER__BOOTSTRAP_SIGNING_SET="$BOOTSTRAP_SIGNER_SET" && cargo run --bin sbtc-signer -- --config $DEVENV/docker-compose/sbtc-signer/signer-config.toml --migrate-db > $PWD/target/signer-1.log 2>&1 &)
   if [ "$1" -ge 2 ]; then
-    RESULT2=$(. $PWD/devenv/local/envs/signer-2.env && SIGNER_SIGNER__BOOTSTRAP_SIGNING_SET="$BOOTSTRAP_SIGNER_SET" && cargo run --bin sbtc-signer -- --config devenv/local/docker-compose/sbtc-signer/signer-config.toml --migrate-db > $PWD/target/signer-2.log 2>&1 &)
+    RESULT2=$(. $DEVENV/envs/signer-2.env && SIGNER_SIGNER__BOOTSTRAP_SIGNING_SET="$BOOTSTRAP_SIGNER_SET" && cargo run --bin sbtc-signer -- --config $DEVENV/docker-compose/sbtc-signer/signer-config.toml --migrate-db > $PWD/target/signer-2.log 2>&1 &)
   fi
   if [ "$1" -ge 3 ]; then
-    RESULT3=$(. $PWD/devenv/local/envs/signer-3.env && SIGNER_SIGNER__BOOTSTRAP_SIGNING_SET="$BOOTSTRAP_SIGNER_SET" && cargo run --bin sbtc-signer -- --config devenv/local/docker-compose/sbtc-signer/signer-config.toml --migrate-db > $PWD/target/signer-3.log 2>&1 &)
+    RESULT3=$(. $DEVENV/envs/signer-3.env && SIGNER_SIGNER__BOOTSTRAP_SIGNING_SET="$BOOTSTRAP_SIGNER_SET" && cargo run --bin sbtc-signer -- --config $DEVENV/docker-compose/sbtc-signer/signer-config.toml --migrate-db > $PWD/target/signer-3.log 2>&1 &)
   fi
 
-  printf "${GRAY}$@ signers started${NC}\n"
+  printf "${GRAY}$* signers started${NC}\n"
 }
 
 main() {
