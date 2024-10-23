34849d2362bb8cbc98568bb2bc09478fcfa910f4
djordon <dan.jordon@gmail.com>
2024-10-23 15:27:40 -0400

    rename the new signer_public_key column in the deposit_requests table

--- signer/src/storage/model.rs
+++ signer/src/storage/model.rs
@@ -78,7 +78,7 @@ pub struct DepositRequest {
     pub lock_time: u64,
     /// The public key used in the deposit script. The signers public key
     /// is for Schnorr signatures.
-    pub signer_public_key: PublicKeyXOnly,
+    pub signers_public_key: PublicKeyXOnly,
     /// The addresses of the input UTXOs funding the deposit request.
     #[cfg_attr(
         feature = "testing",
@@ -105,7 +105,7 @@ impl From<Deposit> for DepositRequest {
             amount: deposit.info.amount,
             max_fee: deposit.info.max_fee,
             lock_time: deposit.info.lock_time,
-            signer_public_key: deposit.info.signers_public_key.into(),
+            signers_public_key: deposit.info.signers_public_key.into(),
             sender_script_pub_keys: sender_script_pub_keys.into_iter().collect(),
         }
     }
--- signer/src/storage/postgres.rs
+++ signer/src/storage/postgres.rs
@@ -527,7 +527,7 @@ impl super::DbRead for PgStore {
               , deposit_requests.amount
               , deposit_requests.max_fee
               , deposit_requests.lock_time
-              , deposit_requests.signer_public_key
+              , deposit_requests.signers_public_key
               , deposit_requests.sender_script_pub_keys
             FROM transactions_in_window transactions
             JOIN sbtc_signer.deposit_requests deposit_requests ON
@@ -586,7 +586,7 @@ impl super::DbRead for PgStore {
               , deposit_requests.amount
               , deposit_requests.max_fee
               , deposit_requests.lock_time
-              , deposit_requests.signer_public_key
+              , deposit_requests.signers_public_key
               , deposit_requests.sender_script_pub_keys
             FROM transactions_in_window transactions
             JOIN sbtc_signer.deposit_requests deposit_requests USING(txid)
@@ -697,7 +697,7 @@ impl super::DbRead for PgStore {
               , requests.amount
               , requests.max_fee
               , requests.lock_time
-              , requests.signer_public_key
+              , requests.signers_public_key
               , requests.sender_script_pub_keys
             FROM sbtc_signer.deposit_requests requests
                  JOIN sbtc_signer.deposit_signers signers
@@ -1393,7 +1393,7 @@ impl super::DbWrite for PgStore {
               , amount
               , max_fee
               , lock_time
-              , signer_public_key
+              , signers_public_key
               , sender_script_pub_keys
               )
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
@@ -1407,7 +1407,7 @@ impl super::DbWrite for PgStore {
         .bind(i64::try_from(deposit_request.amount).map_err(Error::ConversionDatabaseInt)?)
         .bind(i64::try_from(deposit_request.max_fee).map_err(Error::ConversionDatabaseInt)?)
         .bind(i64::try_from(deposit_request.lock_time).map_err(Error::ConversionDatabaseInt)?)
-        .bind(deposit_request.signer_public_key)
+        .bind(deposit_request.signers_public_key)
         .bind(&deposit_request.sender_script_pub_keys)
         .execute(&self.0)
         .await
@@ -1432,7 +1432,7 @@ impl super::DbWrite for PgStore {
         let mut amount = Vec::with_capacity(deposit_requests.len());
         let mut max_fee = Vec::with_capacity(deposit_requests.len());
         let mut lock_time = Vec::with_capacity(deposit_requests.len());
-        let mut signer_public_key = Vec::with_capacity(deposit_requests.len());
+        let mut signers_public_key = Vec::with_capacity(deposit_requests.len());
         let mut sender_script_pubkeys = Vec::with_capacity(deposit_requests.len());
 
         for req in deposit_requests {
@@ -1445,7 +1445,7 @@ impl super::DbWrite for PgStore {
             amount.push(i64::try_from(req.amount).map_err(Error::ConversionDatabaseInt)?);
             max_fee.push(i64::try_from(req.max_fee).map_err(Error::ConversionDatabaseInt)?);
             lock_time.push(i64::try_from(req.lock_time).map_err(Error::ConversionDatabaseInt)?);
-            signer_public_key.push(req.signer_public_key);
+            signers_public_key.push(req.signers_public_key);
             // We need to join the addresses like this (and later split
             // them), because handling of multidimensional arrays in
             // postgres is tough. The naive approach of doing
@@ -1469,7 +1469,7 @@ impl super::DbWrite for PgStore {
             , amount          AS (SELECT ROW_NUMBER() OVER (), amount FROM UNNEST($6::BIGINT[]) AS amount)
             , max_fee         AS (SELECT ROW_NUMBER() OVER (), max_fee FROM UNNEST($7::BIGINT[]) AS max_fee)
             , lock_time       AS (SELECT ROW_NUMBER() OVER (), lock_time FROM UNNEST($8::BIGINT[]) AS lock_time)
-            , signer_pub_keys AS (SELECT ROW_NUMBER() OVER (), signer_public_key FROM UNNEST($9::BYTEA[]) AS signer_public_key)
+            , signer_pub_keys AS (SELECT ROW_NUMBER() OVER (), signers_public_key FROM UNNEST($9::BYTEA[]) AS signers_public_key)
             , script_pub_keys AS (SELECT ROW_NUMBER() OVER (), senders FROM UNNEST($10::VARCHAR[]) AS senders)
             INSERT INTO sbtc_signer.deposit_requests (
                   txid
@@ -1480,7 +1480,7 @@ impl super::DbWrite for PgStore {
                 , amount
                 , max_fee
                 , lock_time
-                , signer_public_key
+                , signers_public_key
                 , sender_script_pub_keys)
             SELECT
                 txid
@@ -1491,7 +1491,7 @@ impl super::DbWrite for PgStore {
               , amount
               , max_fee
               , lock_time
-              , signer_public_key
+              , signers_public_key
               , ARRAY(SELECT decode(UNNEST(regexp_split_to_array(senders, ',')), 'hex'))
             FROM tx_ids
             JOIN output_index USING (row_number)
@@ -1513,7 +1513,7 @@ impl super::DbWrite for PgStore {
         .bind(amount)
         .bind(max_fee)
         .bind(lock_time)
-        .bind(signer_public_key)
+        .bind(signers_public_key)
         .bind(sender_script_pubkeys)
         .execute(&self.0)
         .await
