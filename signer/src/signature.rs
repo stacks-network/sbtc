994889542498a75d5f30bc90b71842ef26250da6
djordon <dan.jordon@gmail.com>
2024-11-01 16:07:30 -0400

    Update WSTS WIP

--- signer/src/signature.rs
+++ signer/src/signature.rs
@@ -12,6 +12,41 @@ use crate::error::Error;
 use crate::keys::PrivateKey;
 use crate::keys::PublicKey;
 
+/// A BIP 340-341 Schnorr proof.
+#[derive(Debug, Clone, Copy)]
+pub struct TaprootSignature(bitcoin::taproot::Signature);
+
+impl From<&bitcoin::taproot::Signature> for TaprootSignature {
+    fn from(value: &bitcoin::taproot::Signature) -> Self {
+        Self(*value)
+    }
+}
+
+impl From<bitcoin::taproot::Signature> for TaprootSignature {
+    fn from(value: bitcoin::taproot::Signature) -> Self {
+        Self(value)
+    }
+}
+
+impl From<TaprootSignature> for bitcoin::taproot::Signature {
+    fn from(value: TaprootSignature) -> Self {
+        value.0
+    }
+}
+
+impl From<wsts::taproot::SchnorrProof> for TaprootSignature {
+    fn from(sig: wsts::taproot::SchnorrProof) -> Self {
+        // This `expect()` is fine since the only requirement for a Schnorr
+        // signature is that it be 64 bytes long. We know this is the case
+        // because [`wsts::taproot::SchnorrProof::to_bytes`] always returns
+        // a 64 byte array.
+        let signature = secp256k1::schnorr::Signature::from_slice(&sig.to_bytes())
+            .expect("We know to_bytes returns 64 bytes");
+        let sighash_type = bitcoin::TapSighashType::Default;
+        Self(bitcoin::taproot::Signature { signature, sighash_type })
+    }
+}
+
 /// For creating signatures.
 pub trait SighashDigest {
     /// The digest to sign.
