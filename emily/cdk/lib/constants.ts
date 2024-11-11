e96902ab35f1ea02c103eb54cd5d085132dfbc83
AshtonStephens <ashton.cs.stephens@gmail.com>
2024-11-11 15:24:30 +0000

    feat: delete persistent data on dev stacks deletion

--- emily/cdk/lib/constants.ts
+++ emily/cdk/lib/constants.ts
@@ -14,4 +14,14 @@ export class Constants {
      * Stage name used for stacks that are intended to only run in a local environment.
      */
     static LOCAL_STAGE_NAME: string = "local";
+
+    /**
+     * Stage name used for stacks that are intended to only run in a development environment.
+     */
+    static DEV_STAGE_NAME: string = "dev";
+
+    /**
+     * Default stage name used when no stage name is provided.
+     */
+    static DEFAULT_STAGE_NAME: string = "dev";
 }
