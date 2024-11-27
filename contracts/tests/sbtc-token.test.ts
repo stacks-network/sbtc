5f966be903a3e1593ca6b8d4bdf1de894f43ffc4
jesus <jnajera1917@gmail.com>
2024-11-27 08:22:20 -0500

    added send-many

--- contracts/tests/sbtc-token.test.ts
+++ contracts/tests/sbtc-token.test.ts
@@ -1,4 +1,4 @@
-import { alice, bob, deployer, deposit, errors, getCurrentBurnInfo, token } from "./helpers";
+import { alice, bob, charlie, deployer, deposit, errors, getCurrentBurnInfo, token } from "./helpers";
 import { test, expect, describe } from "vitest";
 import { txOk, filterEvents, rov, txErr } from "@clarigen/test";
 import { CoreNodeEventType, cvToValue } from "@clarigen/core";
@@ -99,6 +99,76 @@ describe("sBTC token contract", () => {
         bob
       );
       expect(receipt2.value).toEqual(999n);
+    });
+
+    test("Mint & transfer multiple sbtc token", () => {
+      const { burnHeight, burnHash } = getCurrentBurnInfo();
+      const receipt = txOk(
+        deposit.completeDepositWrapper({
+          txid: new Uint8Array(32).fill(0),
+          voutIndex: 0,
+          amount: 1000n,
+          recipient: alice,
+          burnHash,
+          burnHeight,
+          sweepTxid: new Uint8Array(32).fill(1),
+        }),
+        deployer
+      );
+      const printEvents = filterEvents(
+        receipt.events,
+        CoreNodeEventType.ContractEvent
+      );
+      const [print] = printEvents;
+      const printData = cvToValue<{
+        topic: string;
+        txid: string;
+        voutIndex: bigint;
+        amount: bigint;
+      }>(print.data.value);
+      expect(printData).toStrictEqual({
+        topic: "completed-deposit",
+        bitcoinTxid: new Uint8Array(32).fill(0),
+        outputIndex: 0n,
+        amount: 1000n,
+        burnHash,
+        burnHeight: BigInt(burnHeight),
+        sweepTxid: new Uint8Array(32).fill(1),
+      });
+      const receipt1 = txOk(
+        token.transferMany({
+          recipients: [
+            {
+              amount: 100n,
+              sender: alice,
+              to: bob,
+              memo: null
+            },
+            {
+              amount: 100n,
+              sender: alice,
+              to: charlie,
+              memo: null
+            },
+          ],
+        }),
+        alice
+      );
+      expect(receipt1.value).toEqual(2n);
+      const receipt2 = rov(
+        token.getBalance({
+          who: bob,
+        }),
+        bob
+      );
+      expect(receipt2.value).toEqual(100n);
+      const receipt3 = rov(
+        token.getBalance({
+          who: charlie,
+        }),
+        charlie
+      );
+      expect(receipt3.value).toEqual(100n);
     });
 
     test("Fail a non-protocol principal calling protocol-mint", () => {
