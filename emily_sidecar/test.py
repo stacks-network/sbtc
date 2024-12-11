1aaf96b5b02a2c3e150012727a38910443c76b18
Francesco Leacche <francescoleacche@gmail.com>
2024-12-11 14:23:15 +0000

    fix merge

--- emily_sidecar/test.py
+++ emily_sidecar/test.py
@@ -32,13 +32,6 @@ class NewBlockTestCase(unittest.TestCase):
         self.app = app.test_client()
         self.app.testing = True
 
-<<<<<<< HEAD
-    def read_fixture(self, filename):
-        with open(filename, 'r') as file:
-            return json.load(file)
-
-=======
->>>>>>> main
     @patch('requests.post')
     def test_new_block_valid_json(self, mock_post):
         # Mock the response from requests.post
@@ -47,29 +40,10 @@ class NewBlockTestCase(unittest.TestCase):
         mock_response.json.return_value = {"message": "Success"}
         mock_post.return_value = mock_response
 
-<<<<<<< HEAD
-        base_path = os.path.join(os.path.dirname(__file__), "..")
-        fixtures_path = os.path.join(base_path, "signer", "tests", "fixtures")
-        fixtures = [
-            "completed-deposit-event.json",
-            "withdrawal-accept-event.json",
-            "withdrawal-create-event.json",
-            "withdrawal-reject-event.json",
-            "rotate-keys-event.json"
-        ]
-
-        for fixture in fixtures:
-            path = os.path.join(fixtures_path, fixture)
-            data = self.read_fixture(path)
-            response = self.app.post('/new_block', json=data)
-            self.assertEqual(response.status_code, 200)
-        self.assertEqual({}, response.get_json())
-=======
         for fixture in FIXTURES.values():
             response = self.app.post('/new_block', json=fixture)
             self.assertEqual(response.status_code, 200)
             self.assertEqual({}, response.get_json())
->>>>>>> main
 
     def test_new_block_invalid_json(self):
         response = self.app.post('/new_block', data="Not a JSON")
@@ -87,18 +61,7 @@ class NewBlockTestCase(unittest.TestCase):
     def test_new_block_post_request_failure(self, mock_post):
         # Mock the response from requests.post to raise a RequestException
         mock_post.side_effect = requests.RequestException("Failed to send chainstate")
-<<<<<<< HEAD
-
-        base_path = os.path.join(os.path.dirname(__file__), "..")
-        fixtures_path = os.path.join(base_path, "signer", "tests", "fixtures")
-        fixture = "completed-deposit-event.json"
-
-        path = os.path.join(fixtures_path, fixture)
-        data = self.read_fixture(path)
-        response = self.app.post('/new_block', json=data)
-=======
         response = self.app.post('/new_block', json=FIXTURES["complete_deposit"])
->>>>>>> main
         self.assertEqual(response.status_code, 500)
         self.assertIn("Failed to send chainstate", response.get_json()["error"])
 
