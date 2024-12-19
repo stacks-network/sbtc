0bdf446dbf294d7523e78139d28f9d843082f4b2
cylewitruk <cyle.witruk@outlook.com>
2024-12-19 07:49:54 +0100

    naming

---
+++ signer/migrations/0011__add_stacks_related_indexes.sql
@@ -0,0 +1,2 @@
+CREATE INDEX ix_stacks_blocks_block_height ON sbtc_signer.stacks_blocks(block_height DESC);
+CREATE INDEX ix_stacks_transactions_block_hash ON sbtc_signer.stacks_transactions(block_hash DESC);
