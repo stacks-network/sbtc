7987c657221182cdf1e33359de0ec5d0f04fdcef
cylewitruk <cyle.witruk@outlook.com>
2025-01-17 13:41:58 +0100

    wip

--- signer/src/network/libp2p/event_loop.rs
+++ signer/src/network/libp2p/event_loop.rs
@@ -58,10 +58,9 @@ pub async fn run(ctx: &impl Context, swarm: Arc<Mutex<Swarm<SignerBehavior>>>) {
         loop {
             // Poll the libp2p swarm for events, waiting for a maximum of 5ms
             // so that we don't starve the outbox.
-            let event =
-                tokio::time::timeout(Duration::from_millis(5), swarm.lock().await.next())
-                    .await
-                    .unwrap_or_default();
+            let event = tokio::time::timeout(Duration::from_millis(5), swarm.lock().await.next())
+                .await
+                .unwrap_or_default();
 
             // Handle the event if one was received.
             if let Some(event) = event {
