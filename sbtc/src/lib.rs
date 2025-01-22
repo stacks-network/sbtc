eb5882a57bdeb46e5f358b1471b020c6db9a692f
Viktar Makouski <mcviktor@tut.by>
2025-01-22 18:46:26 +0300

    add unit test to prove derivation

--- sbtc/src/lib.rs
+++ sbtc/src/lib.rs
@@ -28,8 +28,17 @@ pub mod testing;
 /// wrote the X-coordinate that is reproduced below.
 ///
 /// [1]: https://github.com/bitcoin/bips/blob/master/bip-0341.mediawiki#constructing-and-spending-taproot-outputs
+#[rustfmt::skip] 
+pub const NUMS_X_COORDINATE: [u8; 32] = [ 
+    0x50, 0x92, 0x9b, 0x74, 0xc1, 0xa0, 0x49, 0x54, 
+    0xb7, 0x8b, 0x4b, 0x60, 0x35, 0xe9, 0x7a, 0x5e, 
+    0x07, 0x8a, 0x5a, 0x0f, 0x28, 0xec, 0x96, 0xd5, 
+    0x47, 0xbf, 0xee, 0x9a, 0xce, 0x80, 0x3a, 0xc0, 
+];
+
+/// This is the pubkey, derived from `NUMS_X_COORDINATE` using the derivation path "0/0".
 #[rustfmt::skip]
-pub const NUMS_X_COORDINATE: [u8; 32] = [
+pub const DERIVED_NUMS_X_COORDINATE: [u8; 32] = [
     0x4a ,0x30 ,0xb2 ,0xe4 ,0x61 ,0xb2 ,0x80, 0xc0,
     0xb1, 0x3a, 0x03, 0x79, 0x90, 0x96, 0xab, 0x12,
     0x56, 0x58, 0x91, 0x53, 0xfc, 0x4b, 0x9c, 0x8c,
@@ -46,4 +55,33 @@ pub const NUMS_X_COORDINATE: [u8; 32] = [
 /// sBTC deposit transactions, this public key is such that it does not
 /// have a known private key.
 pub static UNSPENDABLE_TAPROOT_KEY: LazyLock<XOnlyPublicKey> =
-    LazyLock::new(|| XOnlyPublicKey::from_slice(&NUMS_X_COORDINATE).unwrap());
+    LazyLock::new(|| XOnlyPublicKey::from_slice(&DERIVED_NUMS_X_COORDINATE).unwrap());
+
+mod tests {
+    use super::*;
+    use bitcoin::bip32::{ChainCode, ChildNumber, DerivationPath, Fingerprint, Xpub};
+    use secp256k1::{PublicKey, Secp256k1};
+    use std::str::FromStr;
+
+    /// This test proves, that the DERIVED_NUMS_X_COORDINATE is the correct,
+    /// and is derived from the NUMS_X_COORDINATE using the derivation path "0/0".
+    #[test]
+    fn test_derivation() {
+        let path = DerivationPath::from_str("0/0").unwrap();
+        let secp = Secp256k1::new();
+        let bip32master = Xpub {
+            network: bitcoin::NetworkKind::Main,
+            depth: 0,
+            parent_fingerprint: Fingerprint::from(&[0u8; 4]),
+            child_number: ChildNumber::from_normal_idx(0).unwrap(),
+            public_key: PublicKey::from_x_only_public_key(
+                XOnlyPublicKey::from_slice(&NUMS_X_COORDINATE).unwrap(),
+                secp256k1::Parity::Even,
+            ),
+            chain_code: ChainCode::from(&[0u8; 32]),
+        };
+        let derived_pubkey = bip32master.derive_pub(&secp, &path).unwrap();
+        let derived_bytes = derived_pubkey.to_x_only_pub().serialize();
+        assert_eq!(derived_bytes, DERIVED_NUMS_X_COORDINATE);
+    }
+}
