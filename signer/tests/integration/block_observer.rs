770cd7e3bd4f1568d3afb8a7292397ec69af4b34
djordon <dan.jordon@gmail.com>
2025-02-01 01:59:17 -0500

    Add the removed test back

--- signer/tests/integration/block_observer.rs
+++ signer/tests/integration/block_observer.rs
@@ -1,3 +1,4 @@
+use std::collections::BTreeSet;
 use std::collections::HashSet;
 use std::ops::Deref;
 use std::sync::atomic::AtomicBool;
@@ -24,14 +25,17 @@ use sbtc::testing::regtest;
 use sbtc::testing::regtest::Recipient;
 use signer::bitcoin::utxo::SbtcRequests;
 use signer::bitcoin::utxo::SignerBtcState;
+use signer::block_observer::get_signer_set_and_aggregate_key;
 use signer::context::SbtcLimits;
 use signer::emily_client::EmilyClient;
 use signer::error::Error;
+use signer::keys::PublicKey;
 use signer::keys::SignerScriptPubKey as _;
 use signer::stacks::api::TenureBlocks;
 use signer::storage::model;
 use signer::storage::model::BitcoinBlockHash;
 use signer::storage::model::EncryptedDkgShares;
+use signer::storage::model::RotateKeysTransaction;
 use signer::storage::model::TxOutput;
 use signer::storage::model::TxOutputType;
 use signer::storage::model::TxPrevout;
@@ -51,6 +55,7 @@ use signer::storage::DbRead as _;
 use signer::testing;
 use signer::testing::context::TestContext;
 use signer::testing::context::*;
+use signer::testing::storage::model::TestData;
 use tokio_stream::wrappers::ReceiverStream;
 use url::Url;
 
@@ -903,6 +908,104 @@ async fn next_headers_to_process_ignores_known_headers() {
         .await
         .unwrap();
     assert!(headers.is_empty());
+
+    testing::storage::drop_db(db).await;
+}
+
+/// The [`TxCoordinatorEventLoop::get_signer_set_and_aggregate_key`]
+/// function is supposed to fetch the "current" signing set and the
+/// aggregate key to use for bitcoin transactions. It attempts to get the
+/// latest rotate-keys contract call transaction confirmed on the canonical
+/// Stacks blockchain and falls back to the DKG shares table if no such
+/// transaction can be found.
+///
+/// This tests that we prefer rotate keys transactions if it's available
+/// but will use the DKG shares behavior is indeed the case.
+#[tokio::test]
+async fn get_signer_public_keys_and_aggregate_key_falls_back() {
+    let db = testing::storage::new_test_database().await;
+
+    let mut rng = rand::rngs::StdRng::seed_from_u64(51);
+
+    let ctx = TestContext::builder()
+        .with_storage(db.clone())
+        .with_mocked_clients()
+        .build();
+
+    // We need stacks blocks for the rotate-keys transactions.
+    let test_params = testing::storage::model::Params {
+        num_bitcoin_blocks: 10,
+        num_stacks_blocks_per_bitcoin_block: 1,
+        num_deposit_requests_per_block: 0,
+        num_withdraw_requests_per_block: 0,
+        num_signers_per_request: 0,
+        consecutive_blocks: false,
+    };
+    let test_data = TestData::generate(&mut rng, &[], &test_params);
+    test_data.write_to(&db).await;
+
+    // We always need the chain tip.
+    let chain_tip = db.get_bitcoin_canonical_chain_tip().await.unwrap().unwrap();
+
+    // We have no rows in the DKG shares table and no rotate-keys
+    // transactions, so there should be no aggregate key, since that only
+    // happens after DKG, but we should always know the current signer set.
+    let (maybe_aggregate_key, signer_set) = get_signer_set_and_aggregate_key(&ctx, chain_tip)
+        .await
+        .unwrap();
+    assert!(maybe_aggregate_key.is_none());
+    assert!(!signer_set.is_empty());
+
+    // Alright, lets write some DKG shares into the database. When we do
+    // that the signer set should be considered whatever the signer set is
+    // from our DKG shares.
+    let shares: EncryptedDkgShares = Faker.fake_with_rng(&mut rng);
+    db.write_encrypted_dkg_shares(&shares).await.unwrap();
+
+    let (aggregate_key, signer_set) = get_signer_set_and_aggregate_key(&ctx, chain_tip)
+        .await
+        .unwrap();
+
+    let shares_signer_set: BTreeSet<PublicKey> =
+        shares.signer_set_public_keys.iter().copied().collect();
+
+    assert_eq!(shares.aggregate_key, aggregate_key.unwrap());
+    assert_eq!(shares_signer_set, signer_set);
+
+    // Okay not we write a rotate-keys transaction into the database. To do
+    // that we need the stacks chain tip, and a something in 3 different
+    // tables...
+    let stacks_chain_tip = db.get_stacks_chain_tip(&chain_tip).await.unwrap().unwrap();
+
+    let rotate_keys: RotateKeysTransaction = Faker.fake_with_rng(&mut rng);
+    let transaction = model::Transaction {
+        txid: rotate_keys.txid.into_bytes(),
+        tx: Vec::new(),
+        tx_type: model::TransactionType::RotateKeys,
+        block_hash: stacks_chain_tip.block_hash.into_bytes(),
+    };
+    let tx = model::StacksTransaction {
+        txid: rotate_keys.txid,
+        block_hash: stacks_chain_tip.block_hash,
+    };
+
+    db.write_transaction(&transaction).await.unwrap();
+    db.write_stacks_transaction(&tx).await.unwrap();
+    db.write_rotate_keys_transaction(&rotate_keys)
+        .await
+        .unwrap();
+
+    // Alright, now that we have a rotate-keys transaction, we can check if
+    // it is preferred over the DKG shares table.
+    let (aggregate_key, signer_set) = get_signer_set_and_aggregate_key(&ctx, chain_tip)
+        .await
+        .unwrap();
+
+    let rotate_keys_signer_set: BTreeSet<PublicKey> =
+        rotate_keys.signer_set.iter().copied().collect();
+
+    assert_eq!(rotate_keys.aggregate_key, aggregate_key.unwrap());
+    assert_eq!(rotate_keys_signer_set, signer_set);
 
     testing::storage::drop_db(db).await;
 }
