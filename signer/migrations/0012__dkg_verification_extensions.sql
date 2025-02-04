274237019f9bb5fc6f2a495d5d1a167fa9a46b1e
cylewitruk <cyle.witruk@outlook.com>
2025-02-04 14:02:37 +0100

    migration needs to update block hash/height

--- signer/migrations/0012__dkg_verification_extensions.sql
+++ signer/migrations/0012__dkg_verification_extensions.sql
@@ -24,23 +24,50 @@ ALTER TABLE sbtc_signer.dkg_shares
     ADD COLUMN verified_at_bitcoin_block_height BIGINT DEFAULT NULL,
     ADD CONSTRAINT fk_dkg_shares_bitcoin_block_hash
         FOREIGN KEY (verified_at_bitcoin_block_hash)
-        REFERENCES sbtc_signer.bitcoin_blocks (block_hash);
+        REFERENCES sbtc_signer.bitcoin_blocks (block_hash),
+    ADD CONSTRAINT chk_verified_at
+        CHECK (
+            (dkg_shares_status_id = 1 AND verified_at_bitcoin_block_hash IS NOT NULL AND verified_at_bitcoin_block_height IS NOT NULL) 
+            OR (dkg_shares_status_id <> 1 AND verified_at_bitcoin_block_hash IS NULL AND verified_at_bitcoin_block_height IS NULL)
+        );
 
--- Set all of the current `dkg_shares` to `3` (revoked) to start with.
+-- Set all of the current `dkg_shares` to `3` (revoked) to start with. Confirmed
+-- DKG shares will be updated to `1` (verified) in the next step.
 UPDATE sbtc_signer.dkg_shares
 SET dkg_shares_status_id = 3;
 
 -- Update the `dkg_shares` which have been included in a
--- `rotate_keys_transactions` to `1` (verified). This update is not fork aware,
--- but at the time of writing there is no forks that should be problematic.
-UPDATE sbtc_signer.dkg_shares
-SET dkg_shares_status_id = 1
-WHERE aggregate_key IN (
-    SELECT s.aggregate_key
+-- `rotate_keys_transactions` which can also be tied to a bitcoin block to `1`
+-- (verified) and set the `verified_at_*` fields to the bitcoin block
+-- hash/height corresponding to the block where these were anchored.
+--
+-- This update is not fork aware, but at the time of writing there is no forks
+-- that should be problematic (i.e. we shouldn't have any rotate-keys events
+-- that have been orphaned).
+WITH updated_shares AS (
+    SELECT 
+        s.aggregate_key,
+        bb.block_hash AS verified_at_bitcoin_block_hash,
+        bb.block_height AS verified_at_bitcoin_block_height
     FROM sbtc_signer.dkg_shares s
     INNER JOIN sbtc_signer.rotate_keys_transactions rkt 
         ON s.aggregate_key = rkt.aggregate_key
-);
+    INNER JOIN sbtc_signer.stacks_transactions stx
+        ON rkt.txid = stx.txid
+    INNER JOIN sbtc_signer.stacks_blocks sb
+        ON stx.block_hash = sb.block_hash
+    INNER JOIN sbtc_signer.bitcoin_blocks bb
+        ON sb.bitcoin_anchor = bb.block_hash
+    ORDER BY bb.block_height DESC
+    LIMIT 1
+)
+UPDATE sbtc_signer.dkg_shares
+SET 
+    dkg_shares_status_id = 1,
+    verified_at_bitcoin_block_hash = updated_shares.verified_at_bitcoin_block_hash,
+    verified_at_bitcoin_block_height = updated_shares.verified_at_bitcoin_block_height
+FROM updated_shares
+WHERE sbtc_signer.dkg_shares.aggregate_key = updated_shares.aggregate_key;
 
 -- Make the `dkg_shares_status_id` column `NOT NULL` now that they should all
 -- have a value.
