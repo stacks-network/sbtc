daf49469700ec0f23cad4f34796248b6bbb4b0d3
djordon <dan.jordon@gmail.com>
2025-02-06 13:01:54 -0500

    Update the migration query

--- signer/migrations/0012__dkg_verification_extensions.sql
+++ signer/migrations/0012__dkg_verification_extensions.sql
@@ -1,75 +1,52 @@
--- Enum table for DKG shares status
-CREATE TABLE sbtc_signer.dkg_shares_status (
-    -- The id of the status, not auto-incremented as we want to control the values.
-    id INTEGER PRIMARY KEY,
-    -- The name of the status.
-    key TEXT NOT NULL,
-    -- Brief description of what the status means.
-    description TEXT NOT NULL
-);
 
--- Insert the initial entries.
-INSERT INTO sbtc_signer.dkg_shares_status (id, key, description) VALUES
-    (0, 'PENDING', 'DKG round successful, pending verification via signing round'),
-    (1, 'VERIFIED', 'Successfully verified via signing round'),
-    (2, 'KEY_REVOKED', 'The DKG key has been revoked and should not be used');
+CREATE TYPE sbtc_signer.dkg_shares_status AS ENUM (
+    'unverified',
+    'verified',
+    'failed'
+);
 
 -- Add the new columns to the `dkg_shares` table. We're not adding indexes for
 -- now because the table is so small that the overhead likely outweighs the
 -- benefits.
 ALTER TABLE sbtc_signer.dkg_shares
-    -- Contains the current 
-    ADD COLUMN dkg_shares_status_id INTEGER DEFAULT 0 REFERENCES sbtc_signer.dkg_shares_status (id),
-    ADD COLUMN verified_at_bitcoin_block_hash BYTEA DEFAULT NULL,
-    ADD COLUMN verified_at_bitcoin_block_height BIGINT DEFAULT NULL,
-    ADD CONSTRAINT fk_dkg_shares_bitcoin_block_hash
-        FOREIGN KEY (verified_at_bitcoin_block_hash)
-        REFERENCES sbtc_signer.bitcoin_blocks (block_hash),
-    ADD CONSTRAINT chk_verified_at
-        CHECK (
-            (dkg_shares_status_id = 1 AND verified_at_bitcoin_block_hash IS NOT NULL AND verified_at_bitcoin_block_height IS NOT NULL) 
-            OR (dkg_shares_status_id <> 1 AND verified_at_bitcoin_block_hash IS NULL AND verified_at_bitcoin_block_height IS NULL)
-        );
+    ADD COLUMN dkg_shares_status sbtc_signer.dkg_shares_status,
+    ADD COLUMN started_at_bitcoin_block_hash BYTEA,
+    ADD COLUMN started_at_bitcoin_block_height BIGINT;
 
--- Set all of the current `dkg_shares` to `3` (revoked) to start with. Confirmed
--- DKG shares will be updated to `1` (verified) in the next step.
+
 UPDATE sbtc_signer.dkg_shares
-SET dkg_shares_status_id = 3;
+SET dkg_shares_status_id = 'unverified';
 
--- Update the `dkg_shares` which have been included in a
--- `rotate_keys_transactions` which can also be tied to a bitcoin block to `1`
--- (verified) and set the `verified_at_*` fields to the bitcoin block
--- hash/height corresponding to the block where these were anchored.
---
--- This update is not fork aware, but at the time of writing there is no forks
--- that should be problematic (i.e. we shouldn't have any rotate-keys events
--- that have been orphaned).
-WITH updated_shares AS (
+-- These are all DKG shares associated scriptPubKeys that have been successfully spent
+UPDATE sbtc_signer.dkg_shares
+SET dkg_shares_status = 'verified'
+FROM sbtc_signer.bitcoin_tx_inputs
+WHERE sbtc_signer.dkg_shares.script_pubkey = sbtc_signer.bitcoin_tx_inputs.script_pubkey;
+
+
+-- Fill in the started at bitcoin blockhash and block height. The timestamp
+-- of when we write the DKG shares row to the database should correspond
+-- with the tenure of the block that started the DKG round.
+WITH block_times AS (
     SELECT 
-        s.aggregate_key,
-        bb.block_hash AS verified_at_bitcoin_block_hash,
-        bb.block_height AS verified_at_bitcoin_block_height
-    FROM sbtc_signer.dkg_shares s
-    INNER JOIN sbtc_signer.rotate_keys_transactions rkt 
-        ON s.aggregate_key = rkt.aggregate_key
-    INNER JOIN sbtc_signer.stacks_transactions stx
-        ON rkt.txid = stx.txid
-    INNER JOIN sbtc_signer.stacks_blocks sb
-        ON stx.block_hash = sb.block_hash
-    INNER JOIN sbtc_signer.bitcoin_blocks bb
-        ON sb.bitcoin_anchor = bb.block_hash
-    ORDER BY bb.block_height DESC
-    LIMIT 1
+        bb1.block_hash
+      , bb1.block_height
+      , bb1.created_at
+      , bb2.created_at AS ended_at
+    FROM sbtc_signer.bitcoin_blocks AS bb2
+    JOIN sbtc_signer.bitcoin_blocks AS bb1
+      ON bb2.parent_hash = bb1.block_hash
 )
 UPDATE sbtc_signer.dkg_shares
 SET 
-    dkg_shares_status_id = 1,
-    verified_at_bitcoin_block_hash = updated_shares.verified_at_bitcoin_block_hash,
-    verified_at_bitcoin_block_height = updated_shares.verified_at_bitcoin_block_height
-FROM updated_shares
-WHERE sbtc_signer.dkg_shares.aggregate_key = updated_shares.aggregate_key;
+    started_at_bitcoin_block_hash = block_times.block_hash
+  , started_at_bitcoin_block_height = block_times.block_height
+FROM block_times
+WHERE sbtc_signer.dkg_shares.created_at >= block_times.created_at
+  AND sbtc_signer.dkg_shares.created_at < block_times.ended_at;
 
--- Make the `dkg_shares_status_id` column `NOT NULL` now that they should all
--- have a value.
+-- Make the new column `NOT NULL` now that they should all have a value.
 ALTER TABLE sbtc_signer.dkg_shares
-    ALTER COLUMN dkg_shares_status_id SET NOT NULL;
+    ALTER COLUMN dkg_shares_status SET NOT NULL,
+    ALTER COLUMN started_at_bitcoin_block_hash SET NOT NULL,
+    ALTER COLUMN started_at_bitcoin_block_height SET NOT NULL;
