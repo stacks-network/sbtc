add7e07533111382bb762b3125ee94f89fa9a194
cylewitruk <cyle.witruk@outlook.com>
2025-03-20 11:30:27 +0100

    remove unnecessary safety check in encode

--- sbtc/src/idpack/codec/encoder.rs
+++ sbtc/src/idpack/codec/encoder.rs
@@ -90,13 +90,6 @@ fn encode_bitmap(segment: &Segment) -> Vec<u8> {
     // Populate the bitmap by setting bits for each value (excluding the
     // offset).
     for &value in segment.payload_values() {
-        // Ensure value is greater than offset before subtraction
-        // This defensive check ensures we never panic due to overflow if, for
-        // example, the offset itself is duplicated.
-        if value <= segment.offset() {
-            continue;
-        }
-
         // Convert from value to bit position:
         // 1. Subtract offset to get relative position
         // 2. Subtract 1 more because bit 0 represents (offset+1)
