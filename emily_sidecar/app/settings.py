81ebe0e8e51d5f0a7dd96bb880790ba1797eba36
Francesco Leacche <francescoleacche@gmail.com>
2025-04-04 16:40:46 +0100

    remove unused DEPLOYER_ADDRESS from sidecar

--- emily_sidecar/app/settings.py
+++ emily_sidecar/app/settings.py
@@ -3,8 +3,6 @@ import os
 # Environment variables and configuration
 API_KEY = os.getenv("EMILY_API_KEY", "testApiKey")
 
-DEPLOYER_ADDRESS = os.getenv("DEPLOYER_ADDRESS", "SN3R84XZYA63QS28932XQF3G1J8R9PC3W76P9CSQS")
-
 EMILY_ENDPOINT = os.getenv("EMILY_ENDPOINT", "http://host.docker.internal:3031").removesuffix("/")
 
 NEW_BLOCK_URL = f"{EMILY_ENDPOINT}/new_block"
